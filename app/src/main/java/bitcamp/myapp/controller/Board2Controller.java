2b7c8e3f8ed3782b442e2422cb13c5f4b8c67942
심현우 <hyunwoo@simhyeon-uui-MacBookPro.local>
2024-04-09 12:25:06 +0900

    .

--- app/src/main/java/bitcamp/myapp/controller/Board2Controller.java
+++ app/src/main/java/bitcamp/myapp/controller/Board2Controller.java
@@ -14,15 +14,13 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/board2")
+@SessionAttributes("attachedFiles")
 public class Board2Controller {
 
   private static final Log log = LogFactory.getLog(Board2Controller.class);
